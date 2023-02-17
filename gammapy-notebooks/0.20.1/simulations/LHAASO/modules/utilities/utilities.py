fade3037e48dbfc88c6ab85c57662ccf339c9858
rubensjrcosta <rubensjrcosta@gmail.com>
2023-02-17 12:02:24 -0300

    Update 02/17/23

--- gammapy-notebooks/0.20.1/simulations/LHAASO/modules/utilities/utilities.py
+++ gammapy-notebooks/0.20.1/simulations/LHAASO/modules/utilities/utilities.py
@@ -10,16 +10,27 @@ from pathlib import Path
 # In[ ]:
 
 
-def mkdir_base_child(base_dir, child_dir):
-    '''Creates a directory: base_dir/child_dir and returs the path 
-    mkdir_base_child(base_dir, child_dir)
-    >>> path_child
+def mkdir_sub_directory(parent_directory = None, child_directory = None):
+    '''Creates a directory: parent_directory/child_directory and returs the path 
+    >>>mkdir_sub_directory(parent_directory, directory)
+    path_parent, path_child
     '''
-    path_base = Path(f"{base_dir}")
-    path_base.mkdir(exist_ok=True)
+    if child_directory is None:
 
-    path_child = Path(f"{path_base}/{child_dir}")
-    path_child.mkdir(exist_ok=True)
+        path_parent = Path(f"{parent_directory}")
+        path_parent.mkdir(exist_ok=True)
+        print("Directory '% s' created" % path_parent)
+        
+        return path_parent
     
-    return path_child
+    else:
+        
+        path_parent = Path(f"{parent_directory}")
+        path_parent.mkdir(exist_ok=True)
+
+        path_child = Path(f"{path_parent}/{child_directory}")
+        path_child.mkdir(parents=True, exist_ok=True)
+        print("Directory '% s' created" % path_child)
+
+        return (path_parent, path_child)
 
